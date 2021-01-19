2bceb0938ceb13e1f20e1a4ac7ed1b7a4010d90d
Gideon <gideon.debruijn@student.hu.nl>
2021-01-19 12:55:38 +0100

    added games from json and added labelframe for gamelibrary

--- TkinterGUI.py
+++ TkinterGUI.py
@@ -5,7 +5,7 @@ print("start")
 root = Tk()
 root.title("Steam")
 root.geometry("1000x500")
-root.configure(background="#969696")
+root.configure()
 
 root.grid_rowconfigure(0, minsize=200, weight=1)
 root.grid_columnconfigure(0, minsize=200, weight=1)
@@ -18,13 +18,22 @@ style.configure(
     background="#696969",
 )
 
-
-
 #making two frames for games and friendlist
 friendlist_frame = Frame(root, width=100, height=100, bg="#696969")
 game_frame = Frame(root, width=100, height=100, bg="#696969")
 friendlist_frame.grid(row=0,column=1, sticky="nsew")
 game_frame.grid(row=0,column=0, sticky="nsew")
+
+#temp text in entry field
+def on_entry_click(event):
+    if e.get() == 'Enter a username...':
+       e.delete(0, "end") # delete all the text in the entry
+       e.insert(0, '') #Insert blank for user input
+       e.config(fg = 'black')
+def on_focusout(event):
+    if e.get() == '':
+        e.insert(0, 'Enter a username...')
+        e.config(fg = 'grey')
 
 #friendadd button function
 def button_add():
@@ -61,8 +70,11 @@ def show_stopplaying_button():
 
 #inputfield and add button
 e = Entry(friendlist_frame)
+e.bind('<FocusIn>', on_entry_click)
+e.bind('<FocusOut>', on_focusout)
+e.config(fg = 'grey')
 e.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
-e.insert(0, "Name of Friend")
+e.insert(0, "Enter a username...")
 add_button = Button(friendlist_frame, text="add friend", padx=5, pady=5, command=button_add)
 add_button.grid(row=0, column=2, columnspan=1, padx=2, pady=5)
 
@@ -79,7 +91,7 @@ friendlistfile = open('friendlist.txt','r')
 friendlistreader = friendlistfile.readlines()
 
 for friend in friendlistreader:
-    friendlist.insert(END, friend)
+    friendlist.insert(END, friend.rstrip("\n"))
 
 
 #sort button for friendlist
@@ -93,21 +105,20 @@ current_game_label = ttk.Label(game_frame, text="Currently not playing a game")
 current_game_label.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
 stop_playing_button = Button(game_frame, text="stop game", command=stop_game)
 
-#load gamelist
+#labelframe for library
+labelframe = LabelFrame(game_frame, text="Game Library", font=("TkDefaultFont", 33), bg="#696969")
+labelframe.grid(row=1, rowspan=1, column=0, columnspan=5)
 
+#load gamelist
 gamelistfile = open('gamelist.txt','r')
 gamelist = gamelistfile.readlines()
 
-librarylabel = ttk.Label(game_frame, text="Library", style="TLabel", font=("TkDefaultFont", 33))
-librarylabel.grid(row=1, rowspan=1, column=0, columnspan=2)
 
 #make buttons for each game
 for i in range(len(gamelist)):
-    new_button = Button(game_frame, text=gamelist[i].rstrip("\n"),
+    new_button = Button(labelframe, text=gamelist[i].rstrip("\n"),
                     command=lambda j=i: update_game(gamelist[j].rstrip("\n")))
-    #new_label = Label(root, text="Call of Duty 4")
-    #new_label.grid(row=1, column=4, columnspan=1)
-    new_button.grid(row=i+3, column=1, columnspan=1, padx=2, pady=5)
+    new_button.grid(row=i+3, column=1, columnspan=1, padx=2, pady=2)
 
 root.mainloop()
 print("end")
