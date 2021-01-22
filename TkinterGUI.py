0d0f57ba5a95c8d85be652c2b2421b9cf521d08a
Gideon <gideon.debruijn@student.hu.nl>
2021-01-22 16:13:18 +0100

    styling, added two different libraries

--- TkinterGUI.py
+++ TkinterGUI.py
@@ -28,15 +28,16 @@ root.grid_columnconfigure(1, weight=1)
 style = ttk.Style()
 style.configure(
     "TLabel",
-    background="#696969",
+    background="#1B2838",
+    foreground="grey"
 )
 
 #making two frames for games and friendlist
-library_frame = Frame(root, bg="#696969")
+library_frame = Frame(root, bg="#1B2838")
 library_frame.grid(row=0,column=0, sticky="nsew")
-gameinfo_frame = Frame(root, bg="#696969")
+gameinfo_frame = Frame(root, bg="#1B2838")
 gameinfo_frame.grid(row=0,column=1, sticky="nsew")
-friendlist_frame = Frame(root, bg="#696969")
+friendlist_frame = Frame(root, bg="#1B2838")
 friendlist_frame.grid(row=0,column=2, sticky="nsew")
 
 #temp text in entry field
@@ -53,7 +54,8 @@ def on_focusout(event):
 #friendadd button function
 def button_add():
     friendlist.insert(END, e.get())
-    return
+    ## led veranderen
+    print(friendlist.size())
 
 def sort_friendlist():
     temp_list = list(friendlist.get(0, END))
@@ -66,6 +68,8 @@ def delete_friend():
     try:
         index = friendlist.curselection()[0]
         friendlist.delete(index)
+        ## led veranderen
+        print(friendlist.size())
     except IndexError:
         pass
 
@@ -75,7 +79,7 @@ def update_game(game):
     time_game_started = datetime.now()
     current_game = game
     current_game_label.config(text="Playing: " + game)
-    stop_playing_button.grid(row=0, column=1, columnspan=1, padx=5, pady=5)
+    stop_playing_button.grid(row=1, column=0, columnspan=1, padx=5, pady=5)
     time_played_label.grid_remove()
     show_game_statistics(game)
 
@@ -84,7 +88,7 @@ def stop_game():
     current_game_label.config(text="Currently not playing a game")
     stop_playing_button.grid_remove()
     time_played = (datetime.now() -time_game_started)
-    time_played_label.config(text="Played " + current_game +" for : " + str(time_played).split(".")[0])
+    time_played_label.config(text="Played " + current_game +" for " + str(time_played).split(".")[0])
     time_played_label.grid(row=1, column=0, columnspan=1, padx=5, pady=5)
     hide_game_statistics()
 
@@ -107,7 +111,7 @@ def show_game_statistics(game):
         "\nprice : " + str(displaygame["price"]) +
         "\naverage_playtime : " + str(displaygame["average_playtime"]) + 
         "\nowners : " + str(displaygame["owners"]) +
-        "\ncategories : " + str(displaygame["categories"]) +
+        "\ncategories : " + str(displaygame["categories"][:111]) +
         "\ngenres : " + str(displaygame["genres"]) +
         "\nsteamspy_tags : " + str(displaygame["steamspy_tags"]) +
         "\nplatforms : " + str(displaygame["platforms"])
@@ -116,29 +120,67 @@ def show_game_statistics(game):
         print("Could not find game in steam.json")
     
     gamestats_label.config(text=gamestatsprint)
-    gamestats_label.grid(row=1, column=0, columnspan=4, padx=5, pady=5)
-
-
-
+    gamestats_label.grid(row=2, column=0, columnspan=4, padx=5, pady=5)
 
 def hide_game_statistics():
     gamestats_label.grid_remove()
 
+def onlinelib():
+    global jsondata
+    #make buttons for games in jsondata
+    for widget in labelframe.winfo_children():
+        widget.destroy()
+
+    for i in range(0, 30, 2):
+        jsondata[i]['name']
+        new_button = Button(labelframe, borderwidth=3, fg="grey", bg="#2A3F5A", text=jsondata[i]['name'],
+                        command=lambda j=i: update_game(jsondata[j]['name']))
+        new_button.grid(row=i+3, column=1, columnspan=1, padx=4, pady=0)
+        try:
+            new_button2 = Button(labelframe, borderwidth=3, fg="grey", bg="#2A3F5A", text=jsondata[i+1]['name'],
+                            command=lambda j=i+1: update_game(jsondata[j]['name']))
+            new_button2.grid(row=i+3, column=2, columnspan=1, padx=4, pady=0)
+        except:
+            print("uneven amount of games")
+
+def locallib():
+    #load gamelist
+    gamelistfile = open('gamelist.txt','r')
+    gamelist = gamelistfile.readlines()
+    #make buttons for each game from gamelist.txt
+    for widget in labelframe.winfo_children():
+        widget.destroy()
+
+    for i in range(0, len(gamelist), 2):
+        new_button = Button(labelframe, borderwidth=3, fg="grey", bg="#2A3F5A", text=gamelist[i].rstrip("\n"),
+                        command=lambda j=i: update_game(gamelist[j].rstrip("\n")))
+        new_button.grid(row=i+3, column=1, columnspan=1, padx=4, pady=0)
+        try:
+            new_button2 = Button(labelframe, borderwidth=3, fg="grey", bg="#2A3F5A", text=gamelist[i+1].rstrip("\n"),
+                            command=lambda j=i+1: update_game(gamelist[j].rstrip("\n")))
+            new_button2.grid(row=i+3, column=2, columnspan=1, padx=4, pady=0)
+        except:
+            print("uneven amount of games")
+
+
+#adding labelframe for friends
+friendlabelframe = LabelFrame(friendlist_frame, text="Friends", font=("TkDefaultFont", 33), bg="#1B2838", fg="grey")
+friendlabelframe.grid(row=1, rowspan=1, column=0, columnspan=5)
 
 #inputfield and add button
-e = Entry(friendlist_frame)
+e = Entry(friendlabelframe)
 e.bind('<FocusIn>', on_entry_click)
 e.bind('<FocusOut>', on_focusout)
 e.config(fg = 'grey')
 e.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 e.insert(0, "Enter a username...")
-add_button = Button(friendlist_frame, text="add friend", padx=5, pady=5, command=button_add)
+add_button = Button(friendlabelframe, borderwidth=3, background="green", text="add friend", padx=5, pady=5, command=button_add)
 add_button.grid(row=0, column=2, columnspan=1, padx=2, pady=5)
 
 #friendlist and scrollbar
-friendlist = Listbox(friendlist_frame)
+friendlist = Listbox(friendlabelframe)
 friendlist.grid(row=2, column=0, rowspan=3, columnspan=3, padx=0, pady=0)
-scrollbar = Scrollbar(friendlist_frame)
+scrollbar = Scrollbar(friendlabelframe)
 scrollbar.grid(row=2, column=2, rowspan=3, sticky='ns', padx=0, pady=0)
 friendlist.config(yscrollcommand = scrollbar.set)
 scrollbar.config(command = friendlist.yview)
@@ -152,36 +194,32 @@ for friend in friendlistreader:
 
 
 #sort button for friendlist
-sort_button = Button(friendlist_frame, text="sort A-Z", padx=5, pady=5, command=sort_friendlist)
+sort_button = Button(friendlabelframe, borderwidth=3, fg="grey", bg="#2A3F5A", text="sort A-Z", padx=5, pady=5, command=sort_friendlist)
 sort_button.grid(row=5, column=0, columnspan=1, padx=5, pady=5)
-add_button = Button(friendlist_frame, text="delete friend", padx=5, pady=5, command=delete_friend)
-add_button.grid(row=5, column=2, columnspan=1, padx=5, pady=5)
+del_button = Button(friendlabelframe, borderwidth=3, fg="white", background="red", text="delete friend", padx=5, pady=5, command=delete_friend)
+del_button.grid(row=5, column=2, columnspan=1, padx=5, pady=5)
 
 #playing game display
 current_game_label = ttk.Label(gameinfo_frame, text="Currently not playing a game", font=("TkDefaultFont", 33))
 current_game_label.grid(row=0, column=0, columnspan=1, padx=5, pady=5)
 
 #stop playing button and timeplayed
-stop_playing_button = Button(gameinfo_frame, text="stop game", command=stop_game)
-time_played_label = ttk.Label(gameinfo_frame, text="")
+stop_playing_button = Button(gameinfo_frame,  borderwidth=3, fg="white", bg="red", text="stop game", command=stop_game)
+time_played_label = ttk.Label(gameinfo_frame, font=("TkDefaultFont", 20), text="")
 
 #gamestatistics
 gamestats_label = ttk.Label(gameinfo_frame, text="no statistics found")
 
 #labelframe for library
-labelframe = LabelFrame(library_frame, text="Game Library", font=("TkDefaultFont", 33), bg="#696969")
+labelframe = LabelFrame(library_frame, text="Game Library", font=("TkDefaultFont", 33), bg="#1B2838", fg="grey")
 labelframe.grid(row=1, rowspan=1, column=0, columnspan=5)
+locallib_button = Button(library_frame, text="local library", borderwidth=3, fg="grey", bg="#2A3F5A", padx=5, pady=5, command=locallib)
+locallib_button.grid(row=2, column=0, columnspan=1, padx=2, pady=5)
+onlinelib_button = Button(library_frame, text="online library", borderwidth=3, fg="grey", bg="#2A3F5A", padx=5, pady=5, command=onlinelib)
+onlinelib_button.grid(row=2, column=1, columnspan=1, padx=2, pady=5)
 
-#load gamelist
-gamelistfile = open('gamelist.txt','r')
-gamelist = gamelistfile.readlines()
-
-
-#make buttons for each game
-for i in range(len(gamelist)):
-    new_button = Button(labelframe, text=gamelist[i].rstrip("\n"),
-                    command=lambda j=i: update_game(gamelist[j].rstrip("\n")))
-    new_button.grid(row=i+3, column=1, columnspan=1, padx=2, pady=2)
-
+#loads the online library upon starting the application
+onlinelib()
+    
 root.mainloop()
 print("end")
